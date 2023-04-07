542cc487c62c2f93be5647c40839c983432b8507
dejicranium <itisdeji@gmail.com>
2023-04-07 12:58:17 +0100

    a

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
                         <svg width="330" height="100" viewBox="0 0 330 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M85.09 48.25C85.09 50.39 84.39 52.1 82.99 53.38C81.59 54.66 79.81 55.3 77.65 55.3C75.57 55.3 73.87 54.7 72.55 53.5C71.25 52.28 70.6 50.58 70.6 48.4C70.6 46.42 71.2 44.72 72.4 43.3C73.6 41.88 75.42 41.17 77.86 41.17C80.3 41.17 82.11 41.88 83.29 43.3C84.49 44.7 85.09 46.35 85.09 48.25ZM81.1 48.13C81.1 46.75 80.78 45.74 80.14 45.1C79.5 44.46 78.72 44.14 77.8 44.14C76.8 44.14 76.01 44.5 75.43 45.22C74.87 45.92 74.59 46.91 74.59 48.19C74.59 49.35 74.85 50.33 75.37 51.13C75.91 51.93 76.71 52.33 77.77 52.33C78.77 52.33 79.57 51.96 80.17 51.22C80.79 50.46 81.1 49.43 81.1 48.13ZM88.3131 39.07V35.05H92.1831V39.07H88.3131ZM94.1331 55H86.6631V52.09H88.4631V44.41H86.5131V41.47H92.3331V52.09H94.1331V55ZM112.098 55H104.628V52.09H106.428V47.38C106.428 46.22 106.228 45.39 105.828 44.89C105.448 44.39 104.878 44.14 104.118 44.14C103.198 44.14 102.488 44.43 101.988 45.01C101.488 45.57 101.228 46.31 101.208 47.23V52.09H103.008V55H95.538V52.09H97.338V44.41H95.388V41.47H101.208V43.54C102.108 41.98 103.558 41.2 105.558 41.2C106.838 41.2 107.948 41.58 108.888 42.34C109.828 43.1 110.298 44.28 110.298 45.88V52.09H112.098V55ZM124.631 50.8C124.431 52.36 123.891 53.5 123.011 54.22C122.131 54.94 120.991 55.3 119.591 55.3C118.091 55.3 116.871 54.93 115.931 54.19C115.011 53.43 114.551 52.35 114.551 50.95V44.23H112.631V41.47H114.551V37.36H118.421V41.47H123.281V44.23H118.421V50.29C118.421 51.47 118.961 52.06 120.041 52.06C120.281 52.06 120.601 51.96 121.001 51.76C121.421 51.54 121.691 50.93 121.811 49.93L124.631 50.8ZM137.454 50.86C137.454 52.36 136.854 53.48 135.654 54.22C134.474 54.94 132.934 55.3 131.034 55.3C130.054 55.3 129.064 55.2 128.064 55C127.084 54.78 126.144 54.44 125.244 53.98L125.784 50.2L128.784 50.53V52.21C129.144 52.33 129.524 52.41 129.924 52.45C130.324 52.47 130.624 52.48 130.824 52.48C131.564 52.48 132.204 52.36 132.744 52.12C133.284 51.88 133.554 51.52 133.554 51.04C133.554 50.46 133.144 50.08 132.324 49.9C131.504 49.72 130.554 49.54 129.474 49.36C128.394 49.18 127.444 48.81 126.624 48.25C125.804 47.69 125.394 46.74 125.394 45.4C125.394 43.74 126.034 42.63 127.314 42.07C128.614 41.49 129.954 41.2 131.334 41.2C133.134 41.2 134.824 41.61 136.404 42.43L136.734 45.88L133.734 46.21L133.494 44.32C132.834 44.1 132.124 43.99 131.364 43.99C130.864 43.99 130.434 44.1 130.074 44.32C129.734 44.52 129.564 44.84 129.564 45.28C129.564 45.86 129.954 46.25 130.734 46.45C131.534 46.63 132.454 46.81 133.494 46.99C134.554 47.17 135.474 47.54 136.254 48.1C137.054 48.64 137.454 49.56 137.454 50.86ZM155.821 49.21C155.821 50.77 155.291 52.13 154.231 53.29C153.191 54.43 151.551 55 149.311 55H139.051V51.58H141.151V38.92H139.051V35.5H149.191C150.971 35.5 152.401 35.92 153.481 36.76C154.581 37.58 155.131 38.68 155.131 40.06C155.131 41.08 154.821 41.97 154.201 42.73C153.581 43.49 152.711 43.96 151.591 44.14V44.26C152.991 44.46 154.041 45.01 154.741 45.91C155.461 46.81 155.821 47.91 155.821 49.21ZM150.811 41.11C150.811 40.23 150.541 39.65 150.001 39.37C149.481 39.07 148.861 38.92 148.141 38.92H145.171V43.48H147.841C148.981 43.48 149.761 43.25 150.181 42.79C150.601 42.33 150.811 41.77 150.811 41.11ZM151.501 49.06C151.501 47.42 150.511 46.6 148.531 46.6H145.171V51.58H148.261C150.421 51.58 151.501 50.74 151.501 49.06ZM173.673 55H167.853V52.93C166.953 54.49 165.503 55.27 163.503 55.27C162.223 55.27 161.113 54.89 160.173 54.13C159.233 53.37 158.763 52.19 158.763 50.59V44.38H156.963V41.47H162.633V49.09C162.633 50.27 162.823 51.11 163.203 51.61C163.603 52.09 164.183 52.33 164.943 52.33C165.863 52.33 166.573 52.03 167.073 51.43C167.593 50.83 167.853 50.06 167.853 49.12V44.38H165.663V41.47H171.723V52.09H173.673V55ZM190.772 55H185.102V52.48C184.722 53.26 184.162 53.93 183.422 54.49C182.682 55.03 181.702 55.3 180.482 55.3C178.702 55.3 177.302 54.69 176.282 53.47C175.262 52.25 174.752 50.59 174.752 48.49C174.752 46.47 175.252 44.75 176.252 43.33C177.252 41.91 178.762 41.2 180.782 41.2C181.882 41.2 182.782 41.42 183.482 41.86C184.182 42.3 184.722 42.86 185.102 43.54V37.3H182.402V34.36H188.972V52.09H190.772V55ZM185.102 48.16V47.83C185.102 46.71 184.802 45.84 184.202 45.22C183.622 44.58 182.892 44.26 182.012 44.26C181.152 44.26 180.392 44.58 179.732 45.22C179.092 45.86 178.772 46.87 178.772 48.25C178.772 49.55 179.072 50.54 179.672 51.22C180.272 51.9 180.982 52.24 181.802 52.24C182.822 52.24 183.622 51.81 184.202 50.95C184.802 50.09 185.102 49.16 185.102 48.16Z"
-                                fill="#FFD700" />
+                                fill="#FFEA78" />
                             <circle cx="43" cy="45" r="25" fill="#B79B08" />
                             <ellipse cx="42.1329" cy="45.3012" rx="23.5156" ry="24.0964" fill="#FFD700" />
                             <path
@@ -15,8 +15,10 @@
                                 fill="#FFEA78" stroke="#D8B708" stroke-width="2" />
                             <path opacity="0.5"
                                 d="M51.0552 42.86C51.0552 45.08 50.3252 46.74 48.8652 47.84C47.4252 48.92 45.7452 49.46 43.8252 49.46H40.8252V52.58H43.8252V56H34.7052V52.58H36.8052V39.92H34.7052V36.5H43.6452C46.0652 36.5 47.9052 37.07 49.1652 38.21C50.4252 39.35 51.0552 40.9 51.0552 42.86ZM46.7352 42.92C46.7352 41.96 46.4652 41.22 45.9252 40.7C45.4052 40.18 44.4752 39.92 43.1352 39.92H40.8252V46.07H43.0752C44.4552 46.07 45.4052 45.78 45.9252 45.2C46.4652 44.62 46.7352 43.86 46.7352 42.92Z"
-                                fill="#937C03" />
+                                fill="#413703" />
                         </svg>
+
+
 
                     </div>
 
@@ -28,20 +30,42 @@
                 <div class="hero">
                     <div class="hero__container">
                         <div class="hero__title">
-                            <span style="border-bottom: 3px solid gold;">Gift</span> and <span
-                                style="border-bottom: 3px solid teal;">Earn</span> Points for
+                            <span style="border-bottom: 3px solid #FFEA78;;">Gift</span> and <span
+                                style="border-bottom: 3px solid rgb(1, 155, 155) ;">Earn</span> Points for
                             Discounted Purchases
                         </div>
                         <p class="hero__caption">We enable ventures in the business of hospitality to reward and <span
-                                style="color: #FFD700">retain</span>
+                                style="color: #FFEA78">retain</span>
                             their customers...
                         </p>
-                        <div class="hero__cta" style="display: flex; width: 50%;  justify-content: center">
+                        <div class="hero__cta" style="justify-content: center">
 
-                            <button>Browse Deals</button>
+                            <button>Find Deals</button>
                             <button>Set up a loyalty program</button>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div class="trusted">
+                <div class="trusted__container">
+
+                    <p class="trusted__header">Trusted by</p>
+                    <div class="trusted__logos">
+
+                        <img src="https://theboroughlagos.com/wp-content/uploads/2018/10/Artboard-1-copy.png">
+
+                        <img id="flutterwave-logo" data-v-796721dc=""
+                            src="https://www.afflee.com/_nuxt/img/flutterwave-logo.311915c.svg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer">
+                <div class="footer__container">
+                    <p class="footer__copyright">Copyright 2023, PointsBud Inc</p>
+                    <p>An affiliate of <a style="border-bottom: 1px solid white" href="https://www.afflee.com">Afflee </a>
+                    </p>
                 </div>
             </div>
 
@@ -60,7 +84,7 @@
 }
 
 .header {
-    background-color: #222436;
+    background-color: #1e202d;
     width: 100%;
     min-height: 90vh;
     background-image: url('../static/bg.png');
@@ -119,22 +143,115 @@
         margin: auto;
         letter-spacing: 1px;
 
-        border-radius: 5px;
-        font-size: 20px;
+        border-radius: 50px;
+        font-size: 18px;
+        font-weight: 600;
+        color: $charcoal;
+        margin-bottom: 20px;
+
+        @include media("<=t") {
+            padding: 16px;
+            font-size: 18px;
+            width: 100%;
+        }
+
+        &:hover {
+            box-shadow: rgba(133, 133, 133, 0.25) 0 8px 15px;
+            //transform: translateY(-2px);
+        }
+
 
         &:first-of-type {
-            background: #FFD700 !important;
-            color: black;
+            background: #FFEA78 !important;
             margin-right: 55px;
+
+            @include media("<=t") {
+                margin-right: 18px;
+            }
+
         }
 
         &:last-child {
-            background: teal;
+            background: rgb(1, 155, 155);
             color: white;
+
+            @include media("<=t") {
+                margin-bottom: 60px;
+            }
+
+
         }
     }
 
     &__cta {
         margin: auto;
+        display: flex;
+        width: 50%;
+
+        @include media("<=t") {
+            width: 80%;
+            display: flex;
+            flex-direction: column;
+        }
+
     }
-}</style>
+}
+
+.trusted {
+    background: #FFEA78;
+
+    &__container {
+        display: flex;
+
+        flex-direction: column;
+        align-items: center;
+        padding: 100px 20px;
+        padding-bottom: 150px;
+    }
+
+
+    &__header {
+        color: grey;
+        font-family: 'Red Hat Display' !important;
+        font-weight: 600;
+    }
+
+
+    &__logos {
+        display: grid;
+        grid-template-columns: 40% 40%;
+        align-items: center;
+        justify-content: center;
+        height: 50px;
+    }
+
+    img {
+        filter: contrast(.05);
+        height: 80px;
+
+    }
+
+    #flutterwave-logo {
+        //filter: blur(10px);
+        filter: grayscale(1);
+    }
+}
+
+.footer {
+    background: teal;
+    min-height: 200px;
+    //margin-top: 50px;
+
+    &__container {
+        padding: 150px 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: white;
+        font-family: 'Red Hat Display' !important;
+        font-size: 18px;
+        letter-spacing: 1px;
+
+    }
+}
+</style>
