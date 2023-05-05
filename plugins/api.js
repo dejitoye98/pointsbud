d2a1fdc6698ba392903fc0cfc12a93313ad8948f
dejicranium <itisdeji@gmail.com>
2023-05-05 13:31:36 +0100

    a

--- plugins/api.js
+++ plugins/api.js
@@ -1,4 +1,4 @@
-export default function ({ $axios, store, redirect, $cookies, $config }, inject) {
+export default function ({ app, $axios, $route, store, redirect, $cookies, $config }, inject) {
     const api = $axios.create();
     //api.setBaseURL("//localhost:5000/api/v1");
     api.setBaseURL($config.API_BASE);
@@ -10,5 +10,16 @@ export default function ({ $axios, store, redirect, $cookies, $config }, inject)
         config.headers.common = { ...config.headers.common, ...obj }
         config.headers.common["Content-Type"] = "application/json";
     });
+
+
+    api.onError((error) => {
+        //alert(JSON.stringify($route))
+        if (error?.response?.status == 401) {
+            if (app.router.history.current.path.indexOf('admin') > -1) {
+                app.router.push('/admin')
+            }
+        }
+
+    })
     inject("api", api);
 }
