9713ecc933bfb499b215ec0ebf9eaa3f29bf0321
dejicranium <itisdeji@gmail.com>
2023-05-10 14:14:46 +0100

    a

--- pages/menu/_slug.vue
+++ pages/menu/_slug.vue
@@ -612,7 +612,8 @@
 
 
 
-                    <div class="moving-border" @click="show_recommendation_modal = true">
+                    <div class="moving-border" @click="show_recommendation_modal = true"
+                        v-if="show_recommendation_modal == false">
 
                         <svg width="25" height="30" viewBox="0 0 25 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
