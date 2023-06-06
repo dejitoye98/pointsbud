2bdb69cc4d5a45819db656f8a5b8f3d05d90d6b0
dejicranium <itisdeji@gmail.com>
2023-06-06 03:00:29 +0100

    a

--- pages/menu/_slug.vue
+++ pages/menu/_slug.vue
@@ -587,7 +587,7 @@
                         <!--  <div :data-category="getCategory(product)" @click="showOrderModal(product)" class="product"
                             v-for="(product, index) in getCategoryProducts(category.id)" :key="index"> -->
 
-                        <div :data-category="getCategory(product)" class="product"
+                        <div @click="showOrderModal(product)" :data-category="getCategory(product)" class="product"
                             v-for="(product, index) in getCategoryProducts(category.id)" :key="index">
                             <div class="product__container">
                                 <div class="product__image" v-if="product.thumbnail">
@@ -612,7 +612,7 @@
                     </div>
 
 
-
+                    <!-- 
                     <div class="moving-border" @click="show_recommendation_modal = true"
                         v-if="show_recommendation_modal == false">
 
@@ -646,7 +646,7 @@
                                 fill="black" />
                         </svg>
 
-                        <!--  <button>
+                          <button>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" style="margin-right: 8px;"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path
@@ -667,14 +667,18 @@
                             Complete your order
                             <span> ({{ cart.length }} items)</span>
 
-                        </button>-->
+                        </button>
 
 
 
-                    </div>
+                    </div> -->
 
-                    <div class="bottom-sticky" style="display: none;">
+
+                    <div class="bottom-sticky" style="display:block;">
+
                         <div class="bottom-sticky__container">
+
+                            <!-- 
                             <button>
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
@@ -685,31 +689,31 @@
 
 
 
-                            </button>
+                            </button>-->
 
 
 
-                            <!-- 
+
                             <button v-if="cart && cart.length > 0" @click="viewCart">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" style="margin-right: 8px;"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M2 2H3.74001C4.82001 2 5.67 2.93 5.58 4L4.75 13.96C4.61 15.59 5.89999 16.99 7.53999 16.99H18.19C19.63 16.99 20.89 15.81 21 14.38L21.54 6.88C21.66 5.22 20.4 3.87 18.73 3.87H5.82001"
-                                        stroke="black" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                                        stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"
                                         stroke-linejoin="round" />
                                     <path
                                         d="M16.25 22C16.9404 22 17.5 21.4404 17.5 20.75C17.5 20.0596 16.9404 19.5 16.25 19.5C15.5596 19.5 15 20.0596 15 20.75C15 21.4404 15.5596 22 16.25 22Z"
-                                        stroke="black" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                                        stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"
                                         stroke-linejoin="round" />
                                     <path
                                         d="M8.25 22C8.94036 22 9.5 21.4404 9.5 20.75C9.5 20.0596 8.94036 19.5 8.25 19.5C7.55964 19.5 7 20.0596 7 20.75C7 21.4404 7.55964 22 8.25 22Z"
-                                        stroke="black" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                                        stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"
                                         stroke-linejoin="round" />
-                                    <path d="M9 8H21" stroke="black" stroke-width="1.5" stroke-miterlimit="10"
+                                    <path d="M9 8H21" stroke="black" stroke-width="2" stroke-miterlimit="10"
                                         stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
                                 <span>{{ cart.length }}</span>
-                            </button> -->
+                            </button>
                         </div>
                     </div>
 
