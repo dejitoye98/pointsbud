50d042fbfb973f27e63cb0d0930dfe79986d06af
dejicranium <itisdeji@gmail.com>
2024-04-09 03:13:44 +0100

    a

--- pages/new.vue
+++ pages/new.vue
@@ -3,6 +3,7 @@
         <div class="container">
             <div class="sidebar">
                 <div class="sidebar__container">
+                    <!-- 
                     <div class="sidebar__item">
 
                         <div class="form-input">
@@ -30,14 +31,22 @@
                             <label for="">Reference Links on the Web</label>
                             <textarea v-model="form.reference_links"></textarea>
                         </div>
-                    </div>
+                    </div>---->
+
+                    <!-- 
                     <div class="sidebar__item">
                         <div class="form-input">
                             <label for="">Exhibits</label>
                             <textarea v-model="form.exhibits"></textarea>
                         </div>
-                    </div>
+                    </div>-->
 
+                    <div class="sidebar__item">
+                        <div class="form-input">
+                            <label for="">Academic Qualification/History</label>
+                            <input v-model="form.academic_qualification">
+                        </div>
+                    </div>
                     <div class="sidebar__item">
                         <div class="form-input">
                             <label for="">Chapter to produce</label>
@@ -46,7 +55,7 @@
                     </div>
 
                     <div class="sidebar__item">
-                        <button>Generate</button>
+                        <button @click="generate" :disabled="generating">Generate</button>
                     </div>
 
                 </div>
@@ -55,7 +64,11 @@
             <div class="main">
                 <div class="main__container">
 
-                    <div class="content" v-html="output"></div>
+                    <div class="content">
+                        <VueEditor v-model="output"></VueEditor>
+
+
+                    </div>
 
                 </div>
             </div>
@@ -66,17 +79,31 @@
 
 
 <script>
+import { VueEditor } from "vue2-editor";
+
 export default {
+    components: {
+        VueEditor
+    },
     data() {
         return {
+            
             output: '',
             form: {
                 chapter: 1,
-            }
+            },
+            generating: false
         }
     },
     methods: {
-
+        generate(){
+            this.generating = true
+            this.$api.post('/generate', this.form).then(resp=> {
+                this.output = resp.data.data
+            }).finally(()=> {
+                this.generating = false
+            })
+        }
     },
     watch: {
 
@@ -114,6 +141,8 @@ export default {
 
         button {
             @include smallbutton;
+
+
 
         }
     }
