6f38eb3cda549a9c8bdcf56324297addd35c5f59
dejicranium <itisdeji@gmail.com>
2024-04-09 03:21:19 +0100

    a

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: "AssetBlend",
+        title: "Straight Green Card",
         htmlAttrs: {
             lang: "en",
         },
