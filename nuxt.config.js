b54a9258e439c76149211b569b7f85596c75f72c
dejicranium <itisdeji@gmail.com>
2025-09-21 20:58:50 +0100

    a

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+    target: 'static',
+    generate: {
+      fallback: '404.html'
+    },
     // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
     ssr: false,
     publicRuntimeConfig: {
